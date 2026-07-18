f322fa856fe6e3bc4af450542026a95be943f920
warsi1992 <68843781+warsi1992@users.noreply.github.com>
2026-07-19 01:56:32 +0530

    Add menuClick output event to AdminHeaderComponent

--- src/app/features/admin/layout/admin-header/admin-header.component.ts
+++ src/app/features/admin/layout/admin-header/admin-header.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-admin-header',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './admin-header.component.html',
   styleUrl: './admin-header.component.css'
 })
 export class AdminHeaderComponent {
 
+  @Output() menuClick = new EventEmitter<void>();
+
+  toggleMenu() {
+    this.menuClick.emit();
+  }
 }
